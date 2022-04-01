63023c00849b374a311f526531090644af410d0e
audrbar <audrbar@gmail.com>
2022-04-01 19:11:22 +0300

    first commit

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter} from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
+
+const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+    const router = useRouter();
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            if(item.value && filterValues?.[item.name]) {
+                query[item.name] = item.value
+            }
+        })
+        router.push({ pathname: path, query })
+    }
+
+    return (
+        <Flex bg="gray.100" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                     placeholder={filter.placeholder}
+                     w="fit-content"
+                     p="2"
+                     onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })} 
+                    >
+                    {filter?.items?.map((item) => (
+                        <option value={item.value} key={item.value}>
+                            {item.name}
+                        </option>
+                    ))}
+                    </Select>  
+                </Box>
+            ))}
+        </Flex>
+    )
+};
+
+export default SearchFilters;
